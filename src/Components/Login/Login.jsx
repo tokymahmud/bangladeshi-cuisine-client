fb0c286b0093ea112713d7717406c2a490ae8db5
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-02 23:25:02 +0600

    login page activation work and register page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const handleLogin =event =>{
+        event.preventDefault();
+        const form =event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email,password);
+
+
+    }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
+  <div className="hero-content flex-col md:flex-row-reverse">
+    <div className="text-center md:text-left">
+      <h1 className="text-5xl font-bold"> Please Login now!</h1>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form onSubmit={handleLogin} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -28,7 +38,11 @@ const Login = () => {
         <div className="form-control mt-6">
           <button className="btn btn-primary">Login</button>
         </div>
-      </div>
+      </form>
+      <Link to='/register'>
+      <button className="btn btn-link">Want to explore Bangladeshi Cuisine?</button>
+
+      </Link>
     </div>
   </div>
 </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -3,7 +3,38 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            <h1>please register</h1>
+           
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col md:flex-row-reverse">
+    <div className="text-center md:text-left">
+      <h1 className="text-5xl font-bold"> Please Register!</h1>
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+        </div>
         </div>
     );
 };
