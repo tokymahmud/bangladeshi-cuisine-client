2ab81d9c98d61eef01ebe8588309204a808685f9
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-02 23:37:27 +0600

    registration page first phase

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleRegister =event=>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const img = form.img.value;
+        const password = form.password.value;
+        console.log(email,password,name,img);
+
+
+    }
     return (
         <div>
            
@@ -11,7 +24,19 @@ const Register = () => {
       <h1 className="text-5xl font-bold"> Please Register!</h1>
     </div>
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form onSubmit={handleRegister} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Image URL</span>
+          </label>
+          <input type="text" name='img' placeholder="Image URL" className="input input-bordered" required />
+        </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
@@ -24,11 +49,11 @@ const Register = () => {
           </label>
           <input type="password" name='password' placeholder="password" className="input input-bordered" required />
           <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+            <Link to='/login' className="label-text-alt link link-hover">Already have an account?</Link>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button className="btn btn-primary">Register</button>
         </div>
       </form>
     </div>
