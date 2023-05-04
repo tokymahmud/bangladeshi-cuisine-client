0e4577684cc361fff4c9a7d29520c68d168e98b0
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-04 10:57:53 +0600

    card img added and styled

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -16,22 +16,28 @@ const Home = () => {
         <div>
          <div>
          <div className='flex'>
-                <h1 className='font-bold text-2xl'>Love for <br></br> <span className='text-orange-300 underline '>Food</span></h1>
+          <div>
+          <h1 className='font-bold text-2xl'>Love for <br></br> <span className='text-orange-300 underline '>Food?</span></h1>
+                <br></br>
+                <p>We are seeking a talented and experienced cook/chef. The successful candidate will<br></br> be responsible for preparing a variety of fast-food items, ...</p>
+
+         </div>
+                
                 <div className="h-96 carousel carousel-vertical rounded-box">
   <div className="carousel-item h-full">
-    <img src="https://ibb.co/qxQ4vyP" />
+    <img src="https://img.freepik.com/free-photo/happy-young-cook-uniform-holding-salad_171337-5342.jpg?w=996&t=st=1683175152~exp=1683175752~hmac=445d128b48554c6f9e6dafc06eb076347fb2592f1bffbd62084191d230e037e6" />
   </div> 
   <div className="carousel-item h-full">
-    <img src="https://ibb.co/KsSP7cT" />
+    <img src="https://img.freepik.com/free-vector/chef-character-design_1322-128.jpg?w=740&t=st=1683175186~exp=1683175786~hmac=48be97c59380ee682564ee905938317af8253383beecce1fffceeba8af0e0ab7" />
   </div> 
   <div className="carousel-item h-full">
-    <img src="https://ibb.co/ZHTD5Dz" />
+    <img src="https://img.freepik.com/free-photo/professional-male-chef-cook-white-uniform-cook-hat-pointing-sidewith-index-finger-looking-confident-standing-white-background_141793-20622.jpg?w=996&t=st=1683175217~exp=1683175817~hmac=9d7026849dcc9e1dfd583bc7ec87ef3b0de958e5ce471651938847c1f3552603" />
   </div> 
  
 </div>
             </div>
          </div>
-            <div>
+            <div className='grid grid-cols-3 gap-4 mt-6'>
             {chefInfo.map((ci) => (
                 
 
