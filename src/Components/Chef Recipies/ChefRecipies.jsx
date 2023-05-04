371e96343ff127259a7edaf53148f410067cf3a1
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-05-04 15:32:06 +0600

    recipies added

--- src/Components/Chef Recipies/ChefRecipies.jsx
+++ src/Components/Chef Recipies/ChefRecipies.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const ChefRecipies = () => {
+  
+ 
+
     const details = useLoaderData();
     const {id} = useParams();
     const [ChefId, setChefId]=useState({});
@@ -15,17 +18,50 @@ const ChefRecipies = () => {
             <div>
             <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row">
-    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+    <img src={ChefId.picture_url}className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">{ChefId.name}</h1>
+      <p className="py-6">{ChefId.bio}</p>
+      <p className="py-6">{ChefId.likes} Likes</p>
+      <p className="py-6">Recipies:{ChefId.num_recipes}</p>
+      <p className="py-6">{ChefId.years_of_experience} Years of experience</p>
+      <button className="btn btn-primary">Contact Now!</button>
     </div>
   </div>
 </div>
+
             </div>
-            <h2> service</h2>
-            <h1>{ChefId.name}</h1>
+            <div>
+                <div>
+                <div className='grid grid-cols-3 gap-4 mt-6'>
+            {ChefId.recipes?.map((cis) => (
+                <div key={cis._id}>
+                    
+            <div className="card w-96 bg-base-100 shadow-xl image-full">
+  <div className="card-body">
+    <h2 className="card-title">{cis?.name}</h2>
+    <p><span className='underline underline-offset-1 text-orange-400'>Ingredients:  </span>{cis?.ingredients}</p>
+    <p><span className='underline underline-offset-1 text-orange-400'>cooking method:  </span>{cis?.method}</p>
+    <p><span className='underline underline-offset-1 text-orange-400'>Rating:  </span>{cis?.rating}</p>
+
+    
+                 </div>
+</div>
+
+                </div>
+                
+
+  ))}
+            </div>
+                </div>
+                <div></div>
+                <div></div>
+
+          
+
+            </div>
+         
+          
            
         </div>
     );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
   ))}
             </div>
 
-<div className='mt-6'>
+<div className='mt-6 md:items-center'>
 <h1 className='text-center '>Available Services</h1>
 <div className="drawer">
   <input id="my-drawer" type="checkbox" className="drawer-toggle" />
@@ -78,6 +78,7 @@ const Home = () => {
     <ul className="menu p-4 w-80 bg-base-100 text-base-content">
       <li><a>Find a place?</a></li>
       <li><a>Seek Chef?</a></li>
+      <li><a>Home delivery?</a></li>
       
     </ul>
   </div>
@@ -85,7 +86,43 @@ const Home = () => {
 
         
       </div>
-      <div >
+      <div>
+      <div className="stats shadow">
+  
+  <div className="stat md:items-center">
+    <div className="stat-figure text-primary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+    </div>
+    <div className="stat-title">Total Likes</div>
+    <div className="stat-value text-primary">25.6K</div>
+    <div className="stat-desc">21% more than last month</div>
+  </div>
+  
+  <div className="stat md:items-center">
+    <div className="stat-figure text-secondary">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+    </div>
+    <div className="stat-title">Page Views</div>
+    <div className="stat-value text-secondary">2.6M</div>
+    <div className="stat-desc">21% more than last month</div>
+  </div>
+  
+  <div className="stat md:items-center">
+    <div className="stat-figure text-secondary">
+      <div className="avatar online">
+        <div className="w-16 rounded-full">
+          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+    </div>
+    <div className="stat-value">86%</div>
+    <div className="stat-title">Tasks done</div>
+    <div className="stat-desc text-secondary">31 tasks remaining</div>
+  </div>
+  
+</div>
+      </div>
+      <div className='md:items-center items-center'>
       <h1 className='text-center '>Drop CV for chef!</h1>
 
 
